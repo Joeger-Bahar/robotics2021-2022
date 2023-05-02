d758439526479ad007dc0cea6bbdc112514a6790
joshua-bar <123775427+joshua-bar@users.noreply.github.com>
2023-05-02 13:34:50 -0600

    Add files via upload

---
+++ RA/include/auton.h
@@ -0,0 +1,28 @@
+// // Start of roller
+//   Calibrate();
+//   if (!LimitSwitch.pressing())
+//     Launch();
+//   Forward(13);
+//   Right(90);
+//   Forward(5);
+//   Intake.spinFor(vex::reverse, 0.6, rotationUnits::rev);
+//   // End of roller
+
+//   // Start of intake first disk
+//   // Back(4);
+//   // Right(80);
+//   // Brake();
+//   // Forward(26);
+//   // Brake();
+
+//   // Right(30);
+//   // Back(20);
+//   // Left(13);
+//   // wait(1, sec);
+//   // Back(2);
+//   // Launch();
+//   // Launch();
+//   // Intake.spin(fwd, 2, volt);
+//   // Intake.stop();
+//   // Launch();
+//   // End of disk in high goal
